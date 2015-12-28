f7b57c6d3ab1011c87f7c5958282fcebbcb8ba8f
atulkum <atulcst@gmail.com>
2015-12-27 19:22:50 -0800

    Update util.py

--- util.py
+++ util.py
@@ -23,3 +23,27 @@ def Heap_gain(p, n, class_label, dict_all, num_features = 1000, gain_minimum_bar
     result = [i[1] for i in heap if i[1] != 'gain_bar']
     #print "the length of dll for class %i is %i"%(class_label, len(result))
     return result
+
+#plotting
+
+plt.figsize(12, 6)
+plt.plot([c.mean_validation_score for c in grid_search.cv_scores_], label="validation error")
+plt.plot([c.mean_training_score for c in grid_search.cv_scores_], label="training error")
+plt.xticks(np.arange(6), param_grid['C']); plt.xlabel("C"); plt.ylabel("Accuracy");plt.legend(loc='best');
+
+fix, ax = plt.subplots(1, 3)
+ax[0].matshow(pca.mean_.reshape(8, 8), cmap=plt.cm.Greys)
+ax[1].matshow(pca.components_[0, :].reshape(8, 8), cmap=plt.cm.Greys)
+ax[2].matshow(pca.components_[1, :].reshape(8, 8), cmap=plt.cm.Greys);
+
+
+plt.figsize(16, 10)
+plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y);
+
+from sklearn.cross_validation import cross_val_score
+scores =  cross_val_score(rf, X_train, y_train, cv=5)
+print("scores: %s  mean: %f  std: %f" % (str(scores), np.mean(scores), np.std(scores)))
+
+param_grid = {'C': 10. ** np.arange(-3, 4)}
+grid_search = GridSearchCV(svm, param_grid=param_grid, cv=3, verbose=3, compute_training_score=True)
+
