5d00914ba96ec9fe574a426e3429aedbc85f8569
atulkum <atulcst@gmail.com>
2016-01-31 08:19:13 -0800

    Update tensorflow_regularization.py

--- tensorflow_regularization.py
+++ tensorflow_regularization.py
@@ -55,4 +55,81 @@ class inference_hidden1(BaseTensorFlow):
             else:
                 regularizers = (reg_hidden1 + reg_linear)
                 return (logits, regularizers)
+                
+    def inference_hidden2(images, isEval=None):
+  # Hidden 1
+    with tf.variable_scope('hidden1', reuse=isEval):
+        weights = tf.get_variable("weights", [IMAGE_PIXELS, hidden1_units],
+            initializer=tf.random_normal_initializer(0.0, 1.0 / math.sqrt(float(IMAGE_PIXELS)),
+                          seed=SEED))    
+        biases = tf.get_variable("biases", [hidden1_units],
+            initializer=tf.constant_initializer(0.0))
+    
+        reg_hidden1 = tf.nn.l2_loss(weights) 
+        hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
+        if not isEval:
+            print 'drop out added'
+            hidden1 = tf.nn.dropout(hidden1, 0.5, seed=SEED)
+  # Linear
+    with tf.variable_scope('softmax_linear', reuse=isEval):
+        weights = tf.get_variable("weights", [hidden1_units, NUM_CLASSES],
+            initializer=tf.random_normal_initializer(0.0, 1.0 / math.sqrt(float(hidden1_units)),
+                          seed=SEED))
+        
+        biases = tf.get_variable("biases", [NUM_CLASSES],
+            initializer=tf.constant_initializer(0.0))
+    
+        logits = tf.matmul(hidden1, weights) + biases
+        reg_linear = tf.nn.l2_loss(weights) 
+    
+        if isEval:  
+            return logits
+        else:
+            regularizers = (reg_hidden1 + reg_linear)
+            return (logits, regularizers)
+    
+hidden1_units = 256
+hidden2_units = 128
+def inference_hidden3(images, isEval=None):
+    # Hidden 1
+    with tf.variable_scope('hidden1', reuse=isEval):
+        weights = tf.get_variable("weights", [IMAGE_PIXELS, hidden1_units],
+            initializer=tf.random_normal_initializer(0.0, 1.0 / math.sqrt(float(IMAGE_PIXELS)),
+                          seed=SEED))    
+        biases = tf.get_variable("biases", [hidden1_units],
+            initializer=tf.constant_initializer(0.0))
+    
+        reg_hidden1 = tf.nn.l2_loss(weights) 
+        hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
+        if not isEval:
+            hidden1 = tf.nn.dropout(hidden1, 0.5, seed=SEED)
+    # Hidden 2
+    with tf.variable_scope('hidden2', reuse=isEval):
+        weights = tf.get_variable("weights", [hidden1_units, hidden2_units],
+            initializer=tf.random_normal_initializer(0.0, 1.0 / math.sqrt(float(hidden1_units)),
+                          seed=SEED))    
+        biases = tf.get_variable("biases", [hidden2_units],
+            initializer=tf.constant_initializer(0.0))
+    
+        reg_hidden2 = tf.nn.l2_loss(weights) 
+        hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
+        if not isEval:
+            hidden2 = tf.nn.dropout(hidden2, 0.5, seed=SEED)
+    # Linear
+    with tf.variable_scope('softmax_linear', reuse=isEval):
+        weights = tf.get_variable("weights", [hidden2_units, NUM_CLASSES],
+            initializer=tf.random_normal_initializer(0.0, 1.0 / math.sqrt(float(hidden2_units)),
+                          seed=SEED))
+        biases = tf.get_variable("biases", [NUM_CLASSES],
+            initializer=tf.constant_initializer(0.0))
+    
+        logits = tf.matmul(hidden2, weights) + biases
+        reg_linear = tf.nn.l2_loss(weights) 
+    
+    if isEval:  
+        return logits
+    else:
+        regularizers = (reg_hidden1 +reg_hidden2+ reg_linear)
+        return (logits, regularizers)
+        
         
