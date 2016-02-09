c38643b3ebbc30940e624879b91c85186ee64dbc
atulkum <atulcst@gmail.com>
2016-02-09 12:45:22 -0800

    Update util.py

--- util.py
+++ util.py
@@ -47,3 +47,66 @@ print("scores: %s  mean: %f  std: %f" % (str(scores), np.mean(scores), np.std(sc
 param_grid = {'C': 10. ** np.arange(-3, 4)}
 grid_search = GridSearchCV(svm, param_grid=param_grid, cv=3, verbose=3, compute_training_score=True)
 
+
+#group by frequncy
+df.groupby(['Type','Name']).size()
+df.groupby(['Type','Name'])['Type'].agg({'Frequency':'count'})
+np.bincount()
+
+#check distribution of continuous variables
+from scipy import stats
+#continuous
+stats.kstest(x, 'norm')
+#categorical
+from scipy.stats import chisquare
+chisquare(distA, distB)
+chisquare(distA) #compare with uniform
+
+fig, axes = plt.subplots(nrows=1, ncols=3,figsize=(15, 4))
+country_counts = df_train.country_destination.value_counts()
+country_counts.plot(kind='bar',title='Country Destination Count',ax=axes[0])
+ax = country_counts.div(country_counts.sum()).plot(kind='bar',title='Country Destination %',ax=axes[1] )
+ax.set_yticklabels(['{:3.1f}%'.format(x*100) for x in ax.get_yticks()])
+booked_count = df_train[df_train.country_destination != 'NDF'].country_destination.value_counts()
+ax1 = booked_count.div(booked_count.sum()).plot(kind='bar',title='Country Destination % excl NDF',ax=axes[2] )
+ax1.set_yticklabels(['{:3.1f}%'.format(x*100) for x in ax1.get_yticks()])
+
+
+
+import seaborn as sns
+fig, ax = plt.subplots(nrows=1, ncols=1,figsize=(15, 8))
+sns.boxplot(x='country_destination', y='age', data=df_train, palette="muted", ax =ax)
+ax.set_ylim([10, 60])
+
+
+bar_order = ['NDF','US','other','FR','IT','GB','ES','CA','DE','NL','AU','PT']
+cat_vars = ['gender', 'signup_method', 'signup_flow', 'affiliate_channel', 'affiliate_provider', 
+            'first_affiliate_tracked', 'signup_app', 'first_device_type', 'first_browser', 'language']
+from matplotlib.colors import Colormap
+fig, ax4 = plt.subplots(nrows=2, ncols=4, figsize=(16, 8))
+def pltCatVar(var,axis,ax_num):
+    ctab = pd.crosstab([df_train[var]], df_train.country_destination).apply(lambda x: x/x.sum(), axis=1)
+    ctab[bar_order].plot(kind='bar', stacked=True, ax=axis.reshape(-1)[ax_num],legend=False, 
+                         colormap='terrain')
+for i,var in enumerate(cat_vars[:8]):
+    pltCatVar(var,ax4,i)
+plt.tight_layout()
+fig, ax5 = plt.subplots(nrows=2, ncols=1, figsize=(16, 8), sharey=True)
+for i,var in enumerate(cat_vars[8:]):
+    pltCatVar(var,ax5,i)
+box = ax5[0].get_position()
+ax5[0].set_position([box.x0, box.y0 + box.height * 0.4, box.width, box.height * 0.6])
+ax5[1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.25), ncol=6)
+
+fig, axes = plt.subplots(nrows=1, ncols=1,figsize=(15, 8))
+df_train['date_first_booking'].value_counts().plot(kind='line', ax=axes)
+'''import holidays # this is code to plot the 3 major US summer holidays - the package is not available here
+holidays_tuples = holidays.US(years=[2010,2011,2012,2013])
+popular_holidays = ['Independence Day', 'Labor Day', 'Memorial Day']
+holidays_tuples = {k:v for (k,v) in holidays_tuples.items() if v in popular_holidays}
+us_holidays = pd.to_datetime([i[0] for i in np.array(holidays_tuples.items())])
+for date in us_holidays:
+    axes.annotate('O', (date, df_train[df_train.date_first_booking == date]['date_first_booking'].value_counts()), xytext=(-35, 145), 
+                textcoords='offset points', arrowprops=dict(arrowstyle='wedge'))'''
+fig.autofmt_xdate()
+plt.show()
